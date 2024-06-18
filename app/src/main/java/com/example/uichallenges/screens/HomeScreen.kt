b50d77e1ed01d92e7afeb96d4226080b009f201c
teotia-codes <priyanshu.teotia50@gmail.com>
2024-06-18 18:42:08 +0530

    just a checkpoint 3 composables done so far

--- app/src/main/java/com/example/uichallenges/screens/HomeScreen.kt
+++ app/src/main/java/com/example/uichallenges/screens/HomeScreen.kt
@@ -1,2 +1,159 @@
 package com.example.uichallenges.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.uichallenges.R
+import com.example.uichallenges.ui.theme.ButtonBlue
+import com.example.uichallenges.ui.theme.DarkerButtonBlue
+import com.example.uichallenges.ui.theme.DeepBlue
+import com.example.uichallenges.ui.theme.LightRed
+import com.example.uichallenges.ui.theme.TextWhite
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .background(DeepBlue)
+            .fillMaxSize()
+    ) {
+        Column {
+            GreetingSection()
+            ChipSection(chips = listOf("Sweet sleep", "Insomnia" , "Depression"))
+            CurrentMeditation()
+        }
+    }
+
+
+}
+
+@Composable
+fun GreetingSection() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Good Morning",
+                style = MaterialTheme.typography.headlineLarge
+            )
+            Text(
+                text = "We wish you have a good day",
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+        Icon(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "Search",
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+}
+
+@Composable
+fun ChipSection(
+    chips: List<String>,
+) {
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+    LazyRow {
+        items(chips.size) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                    .clickable {
+                        selectedChipIndex = it
+                    }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(
+                        if (selectedChipIndex == it)
+                            ButtonBlue
+                        else
+                            DarkerButtonBlue
+                    )
+                    .padding(15.dp)
+            )
+            {
+              Text(text = chips[it], color = TextWhite)
+            }
+        }
+    }
+
+}
+
+@Composable
+fun CurrentMeditation(color: Color = LightRed) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(color)
+            .padding(horizontal = 15.dp, vertical = 20.dp)
+            .fillMaxWidth()
+
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Daily Thought",
+                style = MaterialTheme.typography.headlineLarge
+            )
+            Text(
+                text = "Meditation * 3-10 min",
+                style = MaterialTheme.typography.bodySmall,
+                color = TextWhite
+            )
+        }
+        Box (
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .padding(10.dp)
+        ){
+           Icon(painter = painterResource(id = R.drawable.ic_play),
+               contentDescription = "Play",
+               tint = Color.White,
+               modifier = Modifier.size(16.dp))
+        }
+        Icon(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "Search",
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+}
--- app/src/main/java/com/example/uichallenges/ui/theme/Type.kt
+++ app/src/main/java/com/example/uichallenges/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.uichallenges.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.uichallenges.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,6 +17,7 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -31,4 +34,13 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+val gothicA1 = FontFamily(
+    listOf(
+        Font(R.font.gothica1_regular, FontWeight.Normal ),
+        Font(R.font.gothica1_medium, FontWeight.Medium),
+        Font(R.font.gothica1_semibold, FontWeight.SemiBold),
+        Font(R.font.gothica1_bold, FontWeight.Bold),
+        Font(R.font.gothica1_black, FontWeight.Black),
+    )
 )
